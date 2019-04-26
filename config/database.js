d0713253986083050ead08de25041f44ba31d74b
Tikaram Mardi <tikarammardi007@gmail.com>
2019-04-26 22:57:50 +0530

    db config done

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     mongoURI:
-      'mongodb://tikarammardi:5Y44xyRkHP2JKpj@ds147946.mlab.com:47946/note-prod'
+      'mongodb://tikarammardi:5Y44xyRkHP2JKpj@ds147746.mlab.com:47746/note-prod'
   };
 } else {
   module.exports = {
