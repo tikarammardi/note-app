fa04bc3602bb24fb01244d46610abc08337ae333
Tikaram Mardi <tikarammardi007@gmail.com>
2019-04-27 20:55:19 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     mongoURI:
-      'mongodb://tikarammardi:5Y44xyRkHP2JKpj@ds147746.mlab.com:47746/note-prod'
+      'mongodb://tikarammardi:password@ds147746.mlab.com:47746/note-prod'
   };
 } else {
   module.exports = {
